c5d7f8ce09ce325a1f12303983e11436e0510bc1
Ganga Meghanath <gangamegha29@gmail.com>
2018-10-29 07:07:58 +0530

    CNN modified

--- CNN/visualise.m
+++ CNN/visualise.m
@@ -11,3 +11,13 @@ for i=1:20
     h1 = figure(i+20);set(h1, 'Visible', 'off'); imagesc(Wc(:,:,i));colormap(gray);pause(0.5)
     saveas(h1,sprintf('Fig_grey_%d.png',i));
 end
+
+%%
+%Visualize activations
+for i=1:20
+    h=figure(i);set(h, 'Visible', 'off'); imagesc(activations(:,:,i));pause(0.5);
+    saveas(h,sprintf('Fig_act_%d.png',i));
+    h1 = figure(i+20);set(h1, 'Visible', 'off'); imagesc(activations(:,:,i));colormap(gray);pause(0.5)
+    saveas(h1,sprintf('Fig_act_grey_%d.png',i));
+end
+
